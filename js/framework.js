e52846801d3395c03ca3a7e7cac587a1b2f75b6f
NicolasCabral21 <nicolas_cabral21@hotmail.com>
2017-11-12 22:49:03 -0300

    menu con json

--- js/framework.js
+++ js/framework.js
@@ -4,12 +4,14 @@ app.controller("MenuController", ['$scope', 'Services', 'localStorageService', f
 
     var localStorage = localStorageService;
 
+    $scope.createFigure = function() {
         debugger;
         var form = new Object();
 
         form.marker = $scope.marker;
         form.type = "primitive: "+$scope.type;
         form.position = $scope.positionX+" "+ $scope.positionY+" "+ $scope.positionZ;
+        form.color = "color: "+$scope.color;
 
         localStorage.set("form", form);
         window.location.replace("/CamaraApp/framework.html");
@@ -24,11 +26,14 @@ app.controller("ArController", ['$scope', 'localStorageService', function($scope
 
     var form = localStorage.get('form');
 
+    appendObject(form.marker,form.type, form.position, form.color);
 
+    function appendObject(marker,type, position,color) {
         $('<a-entity />', {
             geometry: type,
             position: position,
             material: color,
+            appendTo : $('#' + marker)
         });
     }
 
