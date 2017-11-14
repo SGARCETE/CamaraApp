3aab1e66921d4e00d0997529eaa0d9942f5dac2a
NicolasCabral21 <nicolas_cabral21@hotmail.com>
2017-11-13 22:31:00 -0300

    menu json mejorado (obj-animation)

--- js/framework.js
+++ js/framework.js
@@ -1,17 +1,48 @@
 var app= angular.module('app' ,['LocalStorageModule']);
 
+// CONTROLLER PAG MENU
 app.controller("MenuController", ['$scope', 'Services', 'localStorageService', function($scope, Services, localStorageService){
 
     var localStorage = localStorageService;
 
+    // FILE OBJ
+    $scope.uploadFileObj = function(){
+        $scope.fileObj = event.target.files[0].name;
+    };
+
+    // FILE OBJ
+    $scope.uploadFileMlt = function(){
+        $scope.fileMlt = event.target.files[0].name;
+    };
+
+    // FIGURE
     $scope.createFigure = function() {
         debugger;
         var form = new Object();
-
         form.marker = $scope.marker;
         form.type = "primitive: "+$scope.type;
+        form.radio = $scope.radio;
         form.position = $scope.positionX+" "+ $scope.positionY+" "+ $scope.positionZ;
         form.color = "color: "+$scope.color;
+
+        if($scope.conAnimation){
+            form.animation = {};
+            form.animation.attribute = $scope.attribute;
+            form.animation.duration = $scope.duration;
+            form.animation.repeat = $scope.repeat;
+        }
+
+        localStorage.set("form", form);
+        window.location.replace("/CamaraApp/framework.html");
+    };
+
+    // OBJ
+    $scope.createObj = function() {
+        debugger;
+        var form = new Object();
+        form.marker = $scope.marker;
+        form.fileObj = $scope.fileObj;
+        form.fileMlt = $scope.fileMlt;
 
         localStorage.set("form", form);
         window.location.replace("/CamaraApp/framework.html");
@@ -19,11 +50,10 @@ app.controller("MenuController", ['$scope', 'Services', 'localStorageService', f
 
 }]);
 
+// CONTROLLER PAG FRAMEWOKR
 app.controller("ArController", ['$scope', 'localStorageService', function($scope, localStorageService){
-
+debugger;
     var localStorage = localStorageService;
-    debugger;
-
     var form = localStorage.get('form');
 
     appendObject(form.marker,form.type, form.position, form.color);
@@ -37,6 +67,30 @@ app.controller("ArController", ['$scope', 'localStorageService', function($scope
         });
     }
 
+    function appendObj(id, file, position, scale, marker) {
+        console.log(position);
+        $('<a-entity />', {
+            id: id,
+            position: position,
+            file: file,
+            scale: scale,
+            "obj-model": "obj: url(assets/obj/" + file + ".obj); mtl: url(assets/obj/" + file + ".mtl)",
+            appendTo : $('#' + marker)
+        });
+        document.getElementById(id).setAttribute("position", position);
+    }
+
 }]);
+
+// DIRECTIVE FILE
+app.directive('customOnChange', function() {
+    return {
+        restrict: 'A',
+        link: function (scope, element, attrs) {
+            var onChangeFunc = scope.$eval(attrs.customOnChange);
+            element.bind('change', onChangeFunc);
+        }
+    };
+});
 
 
